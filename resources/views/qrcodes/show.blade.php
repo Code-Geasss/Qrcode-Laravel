c6b101f4d22cbb93a7165f7e94d9a804bf401970
vmundra <krishnamundra91@gmail.com>
2020-04-14 00:32:30 +0530

    made changes in roles model

--- resources/views/qrcodes/show.blade.php
+++ resources/views/qrcodes/show.blade.php
@@ -11,7 +11,7 @@
             <div class="box-body">
                 <div class="row" style="padding-left: 20px">
                     @include('qrcodes.show_fields')
-                    <a href="{{ route('qrcodes.index') }}" class="btn btn-default">Back</a>
+                    
                 </div>
             </div>
         </div>
--- resources/views/qrcodes/show_fields.blade.php
+++ resources/views/qrcodes/show_fields.blade.php
@@ -1,56 +1,75 @@
-<!-- User Id Field -->
-<div class="form-group">
-    {!! Form::label('id', 'Qrcode Id:') !!}
-    <p>{{ $qrcode->id }}</p>
-</div>
+<div class="col-md-6">
 
-<!-- Website Field -->
-<div class="form-group">
-    {!! Form::label('website', 'Website:') !!}
-    <p>{{ $qrcode->website }}</p>
-</div>
+        <!-- Product Name Field -->
+        <div class="form-group">
+            {!! Form::label('product_name', 'Product Name:') !!}
+            <p>{{ $qrcode->product_name }}</p>
+        </div>
 
-<!-- Company Name Field -->
-<div class="form-group">
-    {!! Form::label('company_name', 'Company Name:') !!}
-    <p>{{ $qrcode->company_name }}</p>
-</div>
+        <!-- Amount Field -->
+        <div class="form-group">
+                {!! Form::label('amount', 'Amount:') !!}
+                <p>$ {{ $qrcode->amount }}</p>
+            </div>
 
-<!-- Product Name Field -->
-<div class="form-group">
-    {!! Form::label('product_name', 'Product Name:') !!}
-    <p>{{ $qrcode->product_name }}</p>
-</div>
+         <!-- Product Url Field -->
+         <div class="form-group">
+            {!! Form::label('product_url', 'Product Url:') !!}
+            <p>{{ $qrcode->product_url }}</p>
+        </div>
 
-<!-- Product Url Field -->
-<div class="form-group">
-    {!! Form::label('product_url', 'Product Url:') !!}
-    <p>{{ $qrcode->product_url }}</p>
-</div>
+        @if($qrcode->id == Auth::user()->id || Auth::user()->role_id <3)
+         <!-- User Id Field -->
+        <div class="form-group">
+            {!! Form::label('id', 'Qrcode Id:') !!}
+            <p>{{ $qrcode->id }}</p>
+        </div>
 
-<!-- Callback Url Field -->
-<div class="form-group">
-    {!! Form::label('callback_url', 'Callback Url:') !!}
-    <p>{{ $qrcode->callback_url }}</p>
-</div>
+        <!-- Website Field -->
+        <div class="form-group">
+            {!! Form::label('website', 'Website:') !!}
+            <p>{{ $qrcode->website }}</p>
+        </div>
 
-<!-- Qrcode Path Field -->
-<div class="form-group">
-    {!! Form::label('qrcode_path', 'Qrcode Path:') !!}
-    <p>{{ $qrcode->qrcode_path }}
-    <img src="{{ asset($qrcode->qrcode_path) }}" >
-    </p>
-</div>
+        <!-- Company Name Field -->
+        <div class="form-group">
+            {!! Form::label('company_name', 'Company Name:') !!}
+            <p>{{ $qrcode->company_name }}</p>
+        </div>
 
-<!-- Amount Field -->
-<div class="form-group">
-    {!! Form::label('amount', 'Amount:') !!}
-    <p>{{ $qrcode->amount }}</p>
-</div>
+ 
 
-<!-- Status Field -->
-<div class="form-group">
-    {!! Form::label('status', 'Status:') !!}
-    <p>{{ $qrcode->status }}</p>
-</div>
 
+        <!-- Callback Url Field -->
+        <div class="form-group">
+            {!! Form::label('callback_url', 'Callback Url:') !!}
+            <p>{{ $qrcode->callback_url }}</p>
+        </div>
+
+
+      
+
+        <!-- Status Field -->
+        <div class="form-group">
+            {!! Form::label('status', 'Status:') !!}
+            <p>
+                @if($qrcode->status == 1)
+                    Active
+                @else
+                    Inactive
+                @endif
+                    
+        </div>
+
+    </div>
+@endif
+
+    <div class="col-md-6">
+       <!-- Qrcode Path Field -->
+       <div class="form-group">
+            {!! Form::label('qrcode_path', 'Scan Qrcode:') !!}
+            <p>
+            <img src="{{ asset($qrcode->qrcode_path) }}" >
+            </p>
+        </div>
+    </div>
